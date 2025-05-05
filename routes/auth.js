92e27169947f1bc4bffc0b13a2bf0c03d7cb23f1
MisanthropicCactus <wassimm11@hotmail.com>
2025-05-05 13:45:10 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,10 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// POST /auth/login
+
 router.post('/login', async (req, res) => {
+  console.log(req.body)
   const { email, password } = req.body;
-  
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) return res.status(400).send('Invalid credentials');
@@ -20,12 +20,28 @@ router.post('/login', async (req, res) => {
     res.json({ token });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send(err);
   }
 });
 
 router.post('/signup', async (req, res) => {
-  const { name, email, password } = req.body;
+  console.log('Sing uprequest received:', req.body); 
+  const {
+    firstName , 
+    lastName , 
+    email , 
+    phone , 
+    birthDate , 
+    gender , 
+    licenseNumber , 
+    licenseDate , 
+    password , 
+    confirmPassword , 
+    agreeTerms 
+  } = req.body;
+  if (confirmPassword !==  password   &&   !(typeof password === 'undefined') ) {
+    return res.status(400).json({ message: 'confirmPassword != password' });
+  }
 
   try {
     const existingUser = await User.findOne({ where: { email } });
@@ -34,11 +50,17 @@ router.post('/signup', async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
+    
     const newUser = await User.create({
-      name,
-      email,
-      password: hashedPassword,
+      first_name : firstName  , 
+      last_name : lastName  , 
+      email : email  , 
+      phone : phone  , 
+      password : hashedPassword  , 
+      birth_date : birthDate  , 
+      gender : gender  , 
+      license_number : licenseNumber  ,  
+      license_date :  licenseDate  ,
     });
 
     res.status(201).json({ message: 'User created successfully' });
